635f51bc906e087047b307a745f34b65476dac3d
Castro Hamiltone <castromiltone@gmail.com>
2024-01-18 20:36:51 +0300

    fix

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -12,7 +12,7 @@ from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
-# from api.v1.auth.session_db_auth import SessionDBAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
 from api.v1.auth.session_exp_auth import SessionExpAuth
 
 
